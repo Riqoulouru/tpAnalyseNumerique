2101faf74caa25c965132cb252916beaddd29a82
Barbary-Theo <theo.barbary25@gmail.com>
2022-01-20 14:46:59 +0100

    fefe

--- tp1/Ex2.py
+++ tp1/Ex2.py
@@ -1,6 +1,14 @@
 
+def myhorner(dd, xi, x):
+    n = len(dd)
+    valeur = dd[n - 1]
+    for i in range(n - 2, -1, -1):
+        valeur = valeur * x + dd[i]
+    return valeur
+
+
 def prog():
-    print("oui exo 2 ")
+    print(myhorner([-2, 4, 2, 1/18], [], 4))
 
 
 if __name__=="__main__":
